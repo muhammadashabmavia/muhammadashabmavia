4cbbcae3236634c6ecaa11c77761f837dd17bbec
Muhammad Ashab Mavia <syedashab80@gmail.com>
2025-11-28 05:44:39 +0500

    updated codess

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
+// pages/_app.js
+import '../style.css'       // Your global CSS
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* Your main page component */}
       <Component {...pageProps} />
+
+      {/* Vercel Analytics */}
       <Analytics />
+
+      {/* Vercel Speed Insights */}
+      <SpeedInsights />
     </>
   )
 }
